c42ca980175f7950b9cfdc7b95cadc76704d038d
LENOVO <LENOVO@LENOVO-PC>
2019-12-30 16:15:05 +0530

    Class and object

---
+++ object and class/src/new1/Team.java
@@ -0,0 +1,14 @@
+package new1;
+
+public class Team {
+	String TeamName;
+	String City;
+	public Team(String teamName, String city) {
+		super();
+		this.TeamName = teamName;
+		this.City = city;
+		System.out.println("TeamName "+TeamName+"\n City "+City); 
+	}
+	
+
+}
